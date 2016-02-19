b0adf04e7b970e5d2637b24f873859b691406382
Student <student@RU-012.local>
2016-02-19 14:53:19 -0700

    update

--- main.js
+++
@@ -1,9 +0,0 @@
-var Converter = require("csvtojson").Converter;
-var converter = new Converter({});
-
-converter.on("end_parsed", function (output) {
-   console.log(output);
-});
-
-require("fs").createReadStream("./demo.csv").pipe(converter);
-
